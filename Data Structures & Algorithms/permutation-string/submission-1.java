dc0fe77ca087141258d0092119caf5663026ef56
MahimaRathi2 <mahimarathi36@gmail.com>
2026-05-05 22:23:50 +0530

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int n = s1.length(), m = s2.length();
+
+        if (m < n) return false;
+
+        int[] map1 = new int[26];
+        int[] map2 = new int[26];
+
+        // first window
+        for (int i = 0; i < n; i++) {
+            map1[s1.charAt(i) - 'a']++;
+            map2[s2.charAt(i) - 'a']++;
+        }
+
+        // check first window
+        if (isMatched(map1, map2)) return true;
+
+        // sliding window
+        for (int i = 1; i <= m - n; i++) {
+
+            // remove previous char
+            map2[s2.charAt(i - 1) - 'a']--;
+
+            // add new char
+            map2[s2.charAt(i + n - 1) - 'a']++;
+
+            if (isMatched(map1, map2)) return true;
+        }
+
+        return false;
+    }
+
+    private boolean isMatched(int[] map1, int[] map2) {
+        for (int i = 0; i < 26; i++) {
+            if (map1[i] != map2[i]) return false;
+        }
+        return true;
+    }
+}
